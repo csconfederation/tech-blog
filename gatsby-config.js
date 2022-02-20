d44ed980b8ac6e7b433fa2effbf2724f2152d58d
Steven Young <steven@nightfury.pw>
2022-02-20 13:32:27 -0500

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
-    },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
-    social: {
-      twitter: `kylemathews`,
-    },
+    title: `CSC Technical Committee Blog`,
   },
   plugins: [
     `gatsby-plugin-image`,
@@ -121,7 +112,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/csc-logo.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-react-helmet`,
